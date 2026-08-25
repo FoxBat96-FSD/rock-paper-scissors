a4406b0e9b4a52aeebe84cfd6b5fd2e2267dadab
Wilhelm Blaauw <272574467+FoxBat96-FSD@users.noreply.github.com>
2026-08-25 16:35:02 +0200

    Code getComputerChoice helper function
    getComputerChoice function randomizes between 0 and 1. Values between 0 and 1 equates to rock, paper or scissors.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+function getComputerChoice() {
+  const randomNumber = Math.random();
+  if (randomNumber <= 0.33) return "rock";
+  if (randomNumber <= 0.66) return "paper";
+  else return "scissors";
+}
+
+console.log(getComputerChoice());
